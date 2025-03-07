c1f67a1ff34b8d5cf94705cc2ebca80b38758c49
Prince Kumar <prince.kr9835660@gmail.com>
2025-03-07 13:32:45 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 // MongoDB Connection
-mongoose.connect(MONGO_URI, {   // Replace your MongoDb Connection String with MONGO_URI
+mongoose.connect("MONGO_URI", {   // Replace your MongoDb Connection String with MONGO_URI
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
